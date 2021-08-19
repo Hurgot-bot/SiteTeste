abd4e8aefea1493e7c874102ac2a946b6b82a158
FeliciaShih <fefitinhah@gmail.com>
2021-08-19 18:13:13 -0300

    Atualizada animacao Header

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 var menu = document.getElementById("header-options");
 var logo = document.getElementById("logo");
-
+var menuLinks = document.querySelectorAll("#header-options li");
 
 logo.addEventListener('click',() =>{
     
 
-    if(){
-        menu.style.WebkitAnimationPlayState = "running";
-        
+    if(menu.style.display!="none"){
+        menu.style.display = "none";
+        hideOptions(); 
+        console.log("hide");
     }else{
-        menu.style.WebkitAnimationPlayState = "running";
+        menu.style.display = "";
+        animateOptions();
+        console.log("show");
     }
-    console.log("teste");
-})
+    
+});
+
+ function animateOptions(){
+    console.log("Entered");
+    menuLinks.forEach((link,index) => {
+        
+        setTimeout(() => {
+            link.style.display = "";
+            console.log("Show");
+          }, 30*index);
+        
+    });
+
+    
+}
+
+ function hideOptions(){
+    menuLinks.forEach((link) => {
+        link.style.display = "none";
+    });
+}
+
+
